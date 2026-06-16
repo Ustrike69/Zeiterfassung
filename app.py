d737f3b5d0b637f7382d7b976e8956e7efffedc8
root <root@DevZeiterfassung.local>
2026-06-16 07:44:09 +0000

    feat: v3.0.11 - ProxyFix Cloudflare, Chrome Autofill-Trigger Fix

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from templates import layout as base_layout
 from translations import t, fmt_date as _fmt_date_i18n, fmt_time as _fmt_time_i18n, available_languages as _available_languages
 
 
-APP_VERSION = "v3.0.11.dev2"
+APP_VERSION = "v3.0.11"
 
 IS_DEV = os.environ.get("ZEITERFASSUNG_DEV_MODE") == "1"
 if IS_DEV:
